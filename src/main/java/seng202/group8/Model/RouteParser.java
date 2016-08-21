d2c0b63e2863e759c967d202737934249270ab4f
callumsinclair01 <callumsinclair01@gmail.com>
2016-08-21 12:49:34 +1200

    Implemented RouteParser and test cases

---
+++ src/main/java/seng202/group8/Model/RouteParser.java
@@ -0,0 +1,95 @@
+package seng202.group8.Model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Callum on 21/08/16.
+ */
+public class RouteParser {
+
+    /* Method to separate an input string based off of comma positioning */
+    private ArrayList<String> splitByComma(String inputRoute) {
+        /* Returns a new ArrayList, containing all of the string elements */
+        return new ArrayList<String>(Arrays.asList(inputRoute.split(",")));
+    }
+
+    /* Method to parse a string to an integer with error handling */
+    private int parseToInt(String inputNum) {
+        try {
+            return Integer.parseInt(inputNum);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+
+    /* Temporary method to create an airline for a route */
+    private Airline createAirline(String airlineName, int airlineID) {
+        Airline airline = new Airline();
+        airline.setName(airlineName);
+        airline.setAirlineID(airlineID);
+        return airline;
+    }
+
+    /* Temporary method to create an airport for a route */
+    private Airport createAirport(String airportCode, int airportID) {
+        Airport airport = new Airport();
+
+        if (airportCode.length() == 3) {
+            airport.setIATA(airportCode);
+        } else if (airportCode.length() == 4) {
+            airport.setICAO(airportCode);
+        }
+
+        airport.setAirportID(airportID);
+        return airport;
+    }
+
+    /* Method to check whether an route is codeshared */
+    private void checkCodeshared(Route route, String codeshare) {
+        System.out.println(codeshare);
+        if (codeshare.equals("Y")) {
+            route.setCodeshare(true);
+        } else {
+            route.setCodeshare(false);
+        }
+    }
+
+    private Route createParsedRoute(ArrayList<String> routeInfo) {
+        /* Create the route object */
+        Route route = new Route();
+
+        /** Creates the airline object, later this needs to check if airline
+         * already loaded into the system
+         */
+        Airline airline = createAirline(routeInfo.get(0), Integer.parseInt(routeInfo.get(1)));
+        route.setAirline(airline);
+
+        /** Creates the airport objects for the route, later this also needs to check
+         * whether 'new' airports are already in the system
+         */
+        Airport sourceAirport = createAirport(routeInfo.get(2), parseToInt(routeInfo.get(3)));
+        route.setSourceAirport(sourceAirport);
+
+        Airport destinationAirport = createAirport(routeInfo.get(4), parseToInt(routeInfo.get(5)));
+        route.setDestinationAirport(destinationAirport);
+
+        /* Sets whether the codeshare value is Y */
+        checkCodeshared(route, routeInfo.get(6));
+
+        /* Sets the number of stops the route hase */
+        route.setStops(parseToInt(routeInfo.get(7)));
+
+        /* Sets what plane the route uses */
+        route.setEquipment(routeInfo.get(8));
+
+        return route;
+    }
+
+    /* Public method which is used to create a single route */
+    public Route createSingleRoute(String input) {
+        ArrayList<String> routeInfo = splitByComma(input);
+        return createParsedRoute(routeInfo);
+    }
+
+}
---
+++ src/test/java/seng202/group8/RouteParserTest.java
@@ -0,0 +1,124 @@
+package seng202.group8;
+
+import org.junit.Test;
+import seng202.group8.Model.Airline;
+import seng202.group8.Model.Airport;
+import seng202.group8.Model.Route;
+import seng202.group8.Model.RouteParser;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+
+/**
+ * Created by Callum on 21/08/16.
+ */
+public class RouteParserTest {
+
+    /* String containing IATA information */
+    private String routeString = "2B,410,AER,2965,KZN,2990,Y,10,CR2";
+    /* String containing ICAO information */
+    private String ICAOString = "9W,3000,TRVA,3153,BOMV,2997,,0,73H";
+
+    @Test
+    /* Test to see if the airline is added correctly to a route object */
+    public void testAirlineParsesCorrectly() {
+        RouteParser routeParser = new RouteParser();
+        Route route = routeParser.createSingleRoute(routeString);
+
+        Airline airline = new Airline();
+        airline.setName("2B");
+        airline.setAirlineID(410);
+
+        assertTrue(route.getAirline().getName().equals(airline.getName()));
+        assertTrue(route.getAirline().getAirlineID() == airline.getAirlineID());
+    }
+
+    @Test
+    /* Test to see if the source airport is correctly added to a route object */
+    public void testSourceAirportParsesCorrectly() {
+        RouteParser routeParser = new RouteParser();
+        Route route = routeParser.createSingleRoute(routeString);
+
+        /* IATA TEST CASE */
+        Airport sourceAirport = new Airport();
+        sourceAirport.setIATA("AER");
+        sourceAirport.setAirportID(2965);
+
+        assertEquals(route.getSourceAirport().getIATA(), sourceAirport.getIATA());
+        assertTrue(route.getSourceAirport().getAirportID() == sourceAirport.getAirportID());
+    }
+
+    @Test
+    /* Test to see if the destination airport is correctly added to a route object */
+    public void testDestinationAirportParsesCorrectly() {
+        RouteParser routeParser = new RouteParser();
+        Route route = routeParser.createSingleRoute(routeString);
+
+        /* IATA TEST CASE */
+        Airport destinationAirport = new Airport();
+        destinationAirport.setIATA("KZN");
+        destinationAirport.setAirportID(2990);
+
+        assertEquals(route.getDestinationAirport().getIATA(), destinationAirport.getIATA());
+        assertTrue(route.getDestinationAirport().getAirportID() == destinationAirport.getAirportID());
+    }
+
+    @Test
+    public void testSourceAirportICAOParsesCorrectly() {
+        RouteParser routeParser = new RouteParser();
+        Route route = routeParser.createSingleRoute(ICAOString);
+
+        /* ICAO TEST CASE */
+        Airport sourceAirport = new Airport();
+        sourceAirport.setICAO("TRVA");
+        sourceAirport.setAirportID(3153);
+
+        assertEquals(route.getSourceAirport().getIATA(), sourceAirport.getIATA());
+        assertTrue(route.getSourceAirport().getAirportID() == sourceAirport.getAirportID());
+    }
+
+    @Test
+    public void testDesinationAirportICAOParsesCorrectly() {
+        RouteParser routeParser = new RouteParser();
+        Route route = routeParser.createSingleRoute(ICAOString);
+
+        /* ICAO TEST CASE */
+        Airport destinationAirport = new Airport();
+        destinationAirport.setICAO("BOMV");
+        destinationAirport.setAirportID(2997);
+
+        assertEquals(route.getDestinationAirport().getIATA(), destinationAirport.getIATA());
+        assertTrue(route.getDestinationAirport().getAirportID() == destinationAirport.getAirportID());
+    }
+
+    @Test
+    /* Test to see whether codeshare boolean logic parses correctly */
+    public void testCodeShareWorksCorrectly() {
+        RouteParser routeParser = new RouteParser();
+        Route route = routeParser.createSingleRoute(routeString);
+
+        assertEquals(route.isCodeshare(), true);
+    }
+
+    @Test
+    /* Test to see if the number of stops parses correctly */
+    public void testStopsParsesCorrectly() {
+        RouteParser routeParser = new RouteParser();
+        Route route = routeParser.createSingleRoute(routeString);
+        assertEquals(route.getStops(), 10);
+    }
+
+    @Test
+    /* Test to see whether the equipment specified is added to a route object correctly */
+    public void testEquipmentParsesCorrectly() {
+        RouteParser routeParser = new RouteParser();
+        Route route = routeParser.createSingleRoute(routeString);
+        assertTrue(route.getEquipment().equals("CR2"));
+    }
+
+
+}
+
+
+
+
