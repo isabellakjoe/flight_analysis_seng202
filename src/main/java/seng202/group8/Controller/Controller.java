f80820039ab6e4c3f574ec8cf6e76b202073aae8
Sophie Walton-Smith <sophie.waltonsmith@gmail.com>
2016-08-24 20:47:23 +1200

    Had to merge Izzy and Sophie's Controller.java and gui.fxml files. Added comments for clarity

--- src/main/java/seng202/group8/Controller/Controller.java
+++ src/main/java/seng202/group8/Controller/Controller.java
@@ -35,33 +35,11 @@ import java.util.ResourceBundle;
  * Created by esa46 on 19/08/16.
  */
 public class Controller implements Initializable {
+
     @FXML
     private MenuItem viewAirportData;
-/*
-    public void search(){
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Airport Data File");
-        fileChooser.showOpenDialog(stage);
-    }
-*/
+
     @FXML
-    private TableView<Airline> airlineTable;
-    @FXML
-    private TableColumn<Airline, String> ID;
-    @FXML
-    private TableColumn<Airline, String> name;
-    @FXML
-    private TableColumn<Airline, String> alias;
-    @FXML
-    private TableColumn<Airline, String> IATA;
-    @FXML
-    private TableColumn<Airline, String> ICAO;
-    @FXML
-    private TableColumn<Airline, String> callSign;
-    @FXML
-    private TableColumn<Airline, String> country;
-    @FXML
-    private TableColumn<Airline, String> active;
     private MenuItem addAirportData;
 
     @FXML
@@ -83,26 +61,6 @@ public class Controller implements Initializable {
                 load.getFile(br);
                 load.readData(br);
                 load.buildAirports();
-
-
-    private String airlineString = "324,All Nippon Airways,ANA All Nippon Airways,NH,ANA,ALL NIPPON,Japan,Y";
-    AirlineParser airlineParser = new AirlineParser();
-    Airline airline = airlineParser.createSingleAirline(airlineString);
-
-
-
-    //Initialises the controller class. Automatically called after the fxml file has been loaded
-    @FXML
-    public void initialize(URL location, ResourceBundle resources) {
-        ID.setCellValueFactory(new PropertyValueFactory<Airline, String>("ID"));
-        name.setCellValueFactory(new PropertyValueFactory<Airline, String>("Name"));
-        alias.setCellValueFactory(new PropertyValueFactory<Airline, String>("Alias"));
-        IATA.setCellValueFactory(new PropertyValueFactory<Airline, String>("IATA"));
-        ICAO.setCellValueFactory(new PropertyValueFactory<Airline, String>("ICAO"));
-        callSign.setCellValueFactory(new PropertyValueFactory<Airline, String>("Call Sign"));
-        country.setCellValueFactory(new PropertyValueFactory<Airline, String>("Country"));
-        active.setCellValueFactory(new PropertyValueFactory<Airline, String>("Active"));
-        airlineTable.getItems().setAll(airline);
             }
         } catch(FileNotFoundException ex){
             System.out.println("FILE NOT FOUND");
@@ -150,4 +108,47 @@ public class Controller implements Initializable {
 
     }
 
+
+    @FXML
+    private TableView<Airline> airlineTable;
+    @FXML
+    private TableColumn<Airline, String> ID;
+    @FXML
+    private TableColumn<Airline, String> name;
+    @FXML
+    private TableColumn<Airline, String> alias;
+    @FXML
+    private TableColumn<Airline, String> IATA;
+    @FXML
+    private TableColumn<Airline, String> ICAO;
+    @FXML
+    private TableColumn<Airline, String> callSign;
+    @FXML
+    private TableColumn<Airline, String> country;
+    @FXML
+    private TableColumn<Airline, String> active;
+
+
+    /**
+     * Test Airline to be put in table. Needs to eventually read data already in database
+     * Could be used as a test case
+     */
+    private String airlineString = "324,All Nippon Airways,ANA All Nippon Airways,NH,ANA,ALL NIPPON,Japan,Y";
+    AirlineParser airlineParser = new AirlineParser();
+    Airline airline = airlineParser.createSingleAirline(airlineString);
+
+    //Initialises the controller class. Automatically called after the fxml file has been loaded.
+    //Sets Table Cell values to Airline values
+    @FXML
+    public void initialize(URL location, ResourceBundle resources) {
+        ID.setCellValueFactory(new PropertyValueFactory<Airline, String>("ID"));
+        name.setCellValueFactory(new PropertyValueFactory<Airline, String>("Name"));
+        alias.setCellValueFactory(new PropertyValueFactory<Airline, String>("Alias"));
+        IATA.setCellValueFactory(new PropertyValueFactory<Airline, String>("IATA"));
+        ICAO.setCellValueFactory(new PropertyValueFactory<Airline, String>("ICAO"));
+        callSign.setCellValueFactory(new PropertyValueFactory<Airline, String>("Call Sign"));
+        country.setCellValueFactory(new PropertyValueFactory<Airline, String>("Country"));
+        active.setCellValueFactory(new PropertyValueFactory<Airline, String>("Active"));
+        airlineTable.getItems().setAll(airline);
+    }
 }
---
+++ src/test/java/seng202/group8/ControllerTest.java
@@ -0,0 +1,22 @@
+package seng202.group8;
+
+import org.junit.Test;
+import seng202.group8.Model.Airline;
+import seng202.group8.Model.AirlineParser;
+
+/**
+ * Created by swa158 on 24/08/16.
+ */
+public class ControllerTest {
+
+
+    //Test to see if data displayed in Airline Table is correct
+    @Test public void testAirlineTable() {
+
+        /*private String airlineString = "324,All Nippon Airways,ANA All Nippon Airways,NH,ANA,ALL NIPPON,Japan,Y";
+        AirlineParser airlineParser = new AirlineParser();
+        Airline airline = airlineParser.createSingleAirline(airlineString);
+        */
+
+    }
+}
