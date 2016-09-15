9c853e39fc4c008cf54bd16afea143b2c5254704
cis27 <cis27@uclive.ac.nz>
2016-09-15 12:47:39 +1200

    Refactored Searching Comments

--- src/main/java/seng202/group8/Model/Searchers/AirlineSearcher.java
+++ src/main/java/seng202/group8/Model/Searchers/AirlineSearcher.java
@@ -14,17 +14,27 @@ import java.sql.Connection;
  */
 public class AirlineSearcher {
 
+    // Private list used to contain all loaded airlines
     private ObservableList<Airline> loadedAirlines = FXCollections.observableArrayList();
 
+    /** A constructor for the AirlineSearcher Object
+     *
+     * @param loadedAirlines a observable list of airlines
+     */
     public AirlineSearcher(ObservableList<Airline> loadedAirlines) {
         this.loadedAirlines = loadedAirlines;
     }
 
+    /** Return the list of all currently loaded airlines
+     *
+     * @return an observable list of airlines
+     */
     public ObservableList<Airline> getLoadedAirlines() {
         return loadedAirlines;
     }
 
     private ObservableList<Airline> generateSearchList(String paramID, String searchParam) {
+        // Method to search for airlines through the database
         Database db = new Database();
         DatabaseSearcher dbs = new DatabaseSearcher();
         String sql = dbs.buildAirlineSearch(paramID, searchParam);
@@ -35,42 +45,74 @@ public class AirlineSearcher {
         return airlines;
     }
 
+    /** Method to search for airline based off of ID
+     *
+     * @param airlineID ID of a airline
+     */
     public void airlinesOfID(String airlineID) {
         ObservableList<Airline> matchingAirlines = generateSearchList("airlineid", airlineID);
         System.out.println(matchingAirlines);
         loadedAirlines = matchingAirlines;
     }
 
+    /** Method to search for airline based off of name
+     *
+     * @param name name of a airline
+     */
     public void airlinesOfName(String name) {
         ObservableList<Airline> matchingAirlines = generateSearchList("LOWER(name)", name);
         loadedAirlines = matchingAirlines;
     }
 
+    /** Method to search for a airline based off of alias
+     *
+     * @param alias alias of a airline
+     */
     public void airlinesOfAlias(String alias) {
         ObservableList<Airline> matchingAirlines = generateSearchList("LOWER(alias)", alias);
         loadedAirlines = matchingAirlines;
     }
 
+    /** Method to search for a airline based off of IATA
+     *
+     * @param IATA iata of a airline
+     */
     public void airlinesOfIATA(String IATA) {
         ObservableList<Airline> matchingAirlines = generateSearchList("LOWER(iata)", IATA);
         loadedAirlines = matchingAirlines;
     }
 
+    /** Method to search for a airline based off of ICAO
+     *
+     * @param ICAO icao of a airline
+     */
     public void airlinesOfICAO(String ICAO) {
         ObservableList<Airline> matchingAirlines = generateSearchList("LOWER(icao)", ICAO);
         loadedAirlines = matchingAirlines;
     }
 
+    /** Method to search for a airline based off of Callsign
+     *
+     * @param callsign callsign of a airline
+     */
     public void airlinesOfCallsign(String callsign) {
         ObservableList<Airline> matchingAirlines = generateSearchList("LOWER(callsign)", callsign);
         loadedAirlines = matchingAirlines;
     }
 
+    /** Method to search for a airline based off of Country
+     *
+     * @param country country of a airline
+     */
     public void airlinesOfCountry(String country) {
         ObservableList<Airline> matchingAirlines = generateSearchList("LOWER(country)", country);
         loadedAirlines = matchingAirlines;
     }
 
+    /** Method to search for a airline based off of it's status
+     *
+     * @param activeStatus status of a airline
+     */
     public void airlinesOfActiveStatus(String activeStatus){
         ObservableList<Airline> matchingAirlines = generateSearchList("active", "Y");
         loadedAirlines = matchingAirlines;
--- src/main/java/seng202/group8/Model/Searchers/AirportSearcher.java
+++ src/main/java/seng202/group8/Model/Searchers/AirportSearcher.java
@@ -13,12 +13,21 @@ import java.sql.Connection;
  */
 public class AirportSearcher {
 
+    // Private list used to store all Airport objects
     private ObservableList<Airport> loadedAirports = FXCollections.observableArrayList();
 
+    /** A constructor for the Airport Searcher Object
+     *
+     * @param loadedAirports an observable list of airports
+     */
     public AirportSearcher(ObservableList<Airport> loadedAirports) {
         this.loadedAirports = loadedAirports;
     }
 
+    /** Return the list of all currently loaded airports
+     *
+     * @return an observable list of airports
+     */
     public ObservableList<Airport> getLoadedAirports() {
         return loadedAirports;
     }
@@ -36,61 +45,109 @@ public class AirportSearcher {
 
     }
 
+    /** Method to search for airport based off of ID
+     *
+     * @param airportID id of airport
+     */
     public void airportsOfID(String airportID) {
         ObservableList<Airport> matchingAirports = generateSearchList("airportid", airportID);
         loadedAirports = matchingAirports;
     }
 
+    /** Method to search for airport based off of Name
+     *
+     * @param name name of airport
+     */
     public void airportsOfName(String name) {
         ObservableList<Airport> matchingAirports = generateSearchList("LOWER(name)", name);
         loadedAirports = matchingAirports;
     }
 
+    /** Method to search for airport based off of City
+     *
+     * @param city city of airport
+     */
     public void airportsOfCity(String city) {
         ObservableList<Airport> matchingAirports = generateSearchList("LOWER(city)", city);
         loadedAirports = matchingAirports;
     }
 
+    /** Method to search for airport based off of Country
+     *
+     * @param country country of airport
+     */
     public void airportsOfCountry(String country) {
         ObservableList<Airport> matchingAirports = generateSearchList("LOWER(country)", country);
         loadedAirports = matchingAirports;
     }
 
+    /** Method to search for airport based off of FAA
+     *
+     * @param FAA faa of airport
+     */
     public void airportsOfFAA(String FAA) {
         ObservableList<Airport> matchingAirports = generateSearchList("iata", FAA);
         loadedAirports = matchingAirports;
     }
 
+    /** Method to search for airport based off of IATA
+     *
+     * @param IATA iata of airport
+     */
     public void airportsOfIATA(String IATA) {
         ObservableList<Airport> matchingAirports = generateSearchList("iata", IATA);
         loadedAirports = matchingAirports;
     }
 
+    /** Method to search for airport based off of ICAO
+     *
+     * @param ICAO icao of airport
+     */
     public void airportsOfICAO(String ICAO) {
         ObservableList<Airport> matchingAirports = generateSearchList("icao", ICAO);
         loadedAirports = matchingAirports;
     }
 
+    /** Method to search for airport based off of Latitude
+     *
+     * @param latitude latitude of airport
+     */
     public void airportsOfLatitude(String latitude) {
         ObservableList<Airport> matchingAirports = generateSearchList("latitude", latitude);
         loadedAirports = matchingAirports;
     }
 
+    /** Method to search for airport based off of Longitude
+     *
+     * @param longitude longitude of airport
+     */
     public void airportsOfLongitude(String longitude) {
         ObservableList<Airport> matchingAirports = generateSearchList("longitude", longitude);
         loadedAirports = matchingAirports;
     }
 
+    /** Method to search for airport based off of Altitude
+     *
+     * @param altitude altitude of airport
+     */
     public void airportsOfAltitude(String altitude) {
         ObservableList<Airport> matchingAirports = generateSearchList("altitude", altitude);
         loadedAirports = matchingAirports;
     }
 
+    /** Method to search for airport based off of Timezone
+     *
+     * @param timezone timezone of airport
+     */
     public void airportsOfTimezone(String timezone) {
         ObservableList<Airport> matchingAirports = generateSearchList("timezone", timezone);
         loadedAirports = matchingAirports;
     }
 
+    /** Method to search for airport based off of DST
+     *
+     * @param DST dst of airport
+     */
     public void airportsOfDST(String DST) {
         ObservableList<Airport> matchingAirports = generateSearchList("dst", DST);
         loadedAirports = matchingAirports;
